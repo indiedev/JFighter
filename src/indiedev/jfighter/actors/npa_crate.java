78b3997b3714ca2ea3588abc27c69318597d540a
Praveen <com.java.praveen@gmail.com>
2011-12-11 21:44:13 +0530

    A test class for crate object(np_actor)

---
+++ src/indiedev/jfighter/actors/npa_crate.java
@@ -0,0 +1,12 @@
+package indiedev.jfighter.actors;
+
+import indiedev.jfighter.Helpers.ActorConstants;
+
+public class npa_crate extends AbstractNPlayableActor
+{
+
+	public npa_crate( int xPos) {
+		super("crate",ActorConstants.worldRight,xPos);
+	}
+
+}
