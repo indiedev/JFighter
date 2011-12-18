340f555c77b651fcede4dabc188fd099c7f5a22a
Praveen <com.java.praveen@gmail.com>
2011-12-18 19:15:15 +0530

    Some code refactoring. Added KeyManager class to which the key events of RenderPanel are dispatched.

--- src/indiedev/jfighter/canvas/RenderPanel.java
+++ src/indiedev/jfighter/canvas/RenderPanel.java
@@ -6,7 +6,7 @@ import indiedev.jfighter.actors.AbstractActor;
 import indiedev.jfighter.actors.AbstractNPlayableActor;
 import indiedev.jfighter.actors.AbstractPlayableActor;
 import indiedev.jfighter.actors.TestFan;
-import indiedev.jfighter.controller.PlayableActorMovementListener;
+import indiedev.jfighter.controller.KeyManager;
 
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -14,11 +14,8 @@ import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
-import java.io.InputStream;
 import java.util.ArrayList;
 
-import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
@@ -32,14 +29,13 @@ public class RenderPanel extends JPanel implements ActionListener{
 	//Timer
 	private Timer Timer_repaint;
 	
-	//keylistener for actors
-	PlayableActorMovementListener keyL_PActorHandler;
-	
 	//TESTING
 	TestFan fan=new TestFan();
 	
 	//arrlist containing all actors
-	private final ArrayList<AbstractActor> actorsList=new ArrayList<AbstractActor>();
+	private final ArrayList<AbstractPlayableActor> actorsList=new ArrayList<AbstractPlayableActor>();
+	private final ArrayList<AbstractNPlayableActor> nactorsList=new ArrayList<AbstractNPlayableActor>();
+	private KeyManager keyManager=null;
 	
 	public RenderPanel()
 	{
@@ -61,15 +57,20 @@ public class RenderPanel extends JPanel implements ActionListener{
 		AbstractActor.setY_LIMIT(BI_bgimg.getHeight());
 		System.out.println("Actors Bound assigned");
 
-		keyL_PActorHandler=new PlayableActorMovementListener();
-		addKeyListener(keyL_PActorHandler);
 		setFocusable(true);
 		
-		//initializing Timer(Repaints every 5ms)
-		Timer_repaint=new Timer(5,this);
+		//initializing Timer(Repaints every 16ms i.e:60fps)
+		Timer_repaint=new Timer(16,this);
 		System.out.println("RenderPanel initialized:SIZE[w,h]:"+BI_bgimg.getWidth(null)+" , "+BI_bgimg.getHeight(null));
-		System.out.println("RenderPanel:ActorArray Size:"+actorsList.size());
 		Timer_repaint.start();
+	}
+	
+	//adding keymanager to this panel in order to receive the key events and process the actor
+	public void addKeyManager(KeyManager t_keyManager)
+	{
+		keyManager=t_keyManager;
+		this.addKeyListener(t_keyManager);
+		System.out.println("Added the KeyManager to RenderPanel");
 	}
 	
 	public void paint(Graphics g)
@@ -79,13 +80,18 @@ public class RenderPanel extends JPanel implements ActionListener{
 		
 		//drawing the BG-Image
 		g2d.drawImage(BI_bgimg,0,0,null);
+
+		for(int i=0;i<nactorsList.size();i++)
+		{
+			nactorsList.get(i).drawActor(g2d,true);
+		}
 		
 		//drawing all actors in the array
 		for(int i=0;i<actorsList.size();i++)
 		{
-			actorsList.get(i).drawActor(g2d,false);
-		}
-		fan.draw(g2d,actorsList.get(1).getCurrentImageCoords());
+			actorsList.get(i).drawActor(g2d,true);
+		}	
+		fan.draw(g2d,actorsList.get(0).getCurrentImageCoords());
 	}
 	
 	//called automatically by "Timer" class
@@ -93,24 +99,28 @@ public class RenderPanel extends JPanel implements ActionListener{
 	{
 		repaint();
 
-		//update all actors position
+		//keyboard polling
+		if(keyManager!=null)
+			keyManager.poll();
+		
+		//checks the actor bounds and updates moves it.
 		for(int i=0;i<actorsList.size();i++)
 		{
 			actorsList.get(i).updateActorView();
 		}
+		
 	}
 	
 	public void addNonPlayableActorsToScene(AbstractNPlayableActor t_nActor)
 	{
-		actorsList.add(t_nActor);
+		nactorsList.add(t_nActor);
 		System.out.println("Added a new NActor:"+t_nActor.getActorName());
 	}
 	public void addActorsToScene(AbstractPlayableActor t_actor)
 	{
 		actorsList.add(t_actor);
-		//add to keylistener
-			keyL_PActorHandler.setActorToHandle(t_actor);
 		System.out.println("Added a new Actor:"+t_actor.getActorName());
+		System.out.println("ActorList size:"+actorsList.size());
 	}
 	
 	public void destroy()
